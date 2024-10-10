3a8dc4940694026b1b5901c3518ea5974378ee71
dialva_1998 <dialva_1998@yahoo.com>
2024-10-09 21:27:35 -0500

    Adding video structures

---
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -0,0 +1,10 @@
+package culturemedia.repository;
+import java.util.List;
+import culturemedia.model.Video;
+
+public interface VideoRepository {
+    List<Video> findAll(); // listar todos los videos
+    Video save(Video save); // agregar video
+    List<Video> find(String title); // encontrar un video por titulo
+    List<Video> find(Double fromDuration, Double toDuration) // encontrar un video por duracion
+}
