8b838ae0b6bbc8c19649b5af96ba18819968cfa2
dialva_1998 <dialva_1998@yahoo.com>
2024-10-16 21:36:51 -0500

    Adding video-views structures

--- src/main/java/culturemedia/repository/VideoRepository.java
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -6,5 +6,5 @@ public interface VideoRepository {
     List<Video> findAll(); // listar todos los videos
     Video save(Video save); // agregar video
     List<Video> find(String title); // encontrar un video por titulo
-    List<Video> find(Double fromDuration, Double toDuration) // encontrar un video por duracion
+    List<Video> find(Double fromDuration, Double toDuration); // encontrar un video por duracion
 }
