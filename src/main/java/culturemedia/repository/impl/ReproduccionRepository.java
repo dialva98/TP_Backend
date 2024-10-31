b479633f95de9023adb255c381d5bcd789785b50
dialva98 <70226980+dialva98@users.noreply.github.com>
2024-10-30 20:43:11 -0500

    R10 adding–controller (#10)
    * Adding controller
    
    * Adding controllers
    
    * Adjusting after code review

---
+++ src/main/java/culturemedia/repository/impl/ReproduccionRepository.java
@@ -0,0 +1,7 @@
+package culturemedia.repository.impl;
+import culturemedia.model.Reproduccion;
+
+public interface ReproduccionRepository {
+    Reproduccion save(Reproduccion reproduccion); // NO idea
+
+}
